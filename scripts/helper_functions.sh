f0ba8ec193e4a30ce959e3b02bfdffe03cbad113
Marios <touloupos@gmail.com>
2022-02-11 00:10:06 +0200

    Remove stellar genesis from compose and wait before dpeloying horizon

--- scripts/helper_functions.sh
+++ scripts/helper_functions.sh
@@ -153,7 +153,8 @@ function prepare_dbs(){
 	#Prepare DBs for each validator and generate genesis history archive to be served from the history publisher
 	docker run --rm --network=$TESTNET_NAME -v "$STELLAR_CONF:/etc/stellar/" stellar/stellar-core:latest new-db
 	docker run --rm --network=$TESTNET_NAME -v "$STELLAR_CONF:/etc/stellar/" -v "$WORKING_DIR/deployment/history:/mnt/stellar-hist/stellar-core-archive/node_001/" stellar/stellar-core:latest new-hist local
-	  
+	docker run --name stellar-genesis -p 11626:11626 -p 11625:11625 --network=$TESTNET_NAME -v "$STELLAR_CONF:/etc/stellar/" -v "$WORKING_DIR/deployment/history:/mnt/stellar-hist/stellar-core-archive/node_001/" stellar/stellar-core:latest run
+
 	for (( i=0;i<${num_of_validators};i++ ))
 	do	
 		docker run --rm --network=$TESTNET_NAME -v "$WORKING_DIR/configs/validator-$i:/etc/stellar/" stellar/stellar-core:latest new-db
